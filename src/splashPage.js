09e531eee3833829100ac046b55826811b324d23
kendocode <git@kendocoding.com>
2018-10-12 07:31:16 -0500

    Install redux and friends; make splash page

--- src/splashPage.js
+++ src/splashPage.js
@@ -1,23 +1,25 @@
-
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './splashPage.css';
 
-class App extends Component {
-
+class SplashPage extends Component {
   getEnvValues() {
-    if (!process.env.REACT_APP_EMPLOYEE_NAME || !process.env.REACT_APP_POSITION) {
-      throw new Error('Please define `REACT_APP_EMPLOYEE_NAME` and `REACT_APP_POSITION` in your .env file');
+    if (
+      !process.env.REACT_APP_EMPLOYEE_NAME ||
+      !process.env.REACT_APP_POSITION
+    ) {
+      throw new Error(
+        'Please define `REACT_APP_EMPLOYEE_NAME` and `REACT_APP_POSITION` in your .env file'
+      );
     }
 
-    const employeeName = process.env.REACT_APP_EMPLOYEE_NAME
+    const employeeName = process.env.REACT_APP_EMPLOYEE_NAME;
     const position = process.env.REACT_APP_POSITION;
 
     return { employeeName, position };
   }
 
   render() {
-
     const { employeeName, position } = this.getEnvValues();
 
     return (
@@ -27,12 +29,14 @@ class App extends Component {
           <h2>Welcome to Britebee Development</h2>
         </div>
         <p className="App-intro">
-           <b> Employee Name: { employeeName } </b><br/><br/>
-           <b> Position: { position } </b>
+          <b> Employee Name: {employeeName} </b>
+          <br />
+          <br />
+          <b> Position: {position} </b>
         </p>
       </div>
     );
   }
 }
 
-export default App;
+export default SplashPage;
